a65bfd575f9580d3eab55fa134a18c8dedaab253
lloyduche <eugeneuche93@gmail.com>
2021-11-21 11:03:34 +0100

    changes

---
+++ LewyApi/Lewy.Infrastructure/LewyContext.cs
@@ -0,0 +1,18 @@
+﻿using Lewy.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lewy.Infrastructure
+{
+    public class LewyContext : DbContext
+    {
+        public DbSet<AppUser> AppUsers { get; set; }
+        public LewyContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ LewyApi/Lewy.Infrastructure/Migrations/20211121100132_init.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lewy.Infrastructure.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    UserName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+        }
+    }
+}
